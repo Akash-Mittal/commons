27a223d78d23a46bba18e59e9d06b3fdb5cc3f9a
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-12 09:41:47 +0530

    Changes:1.) Increemental Validations

---
+++ src/main/java/com/am/innovations/validations/Validators.java
@@ -0,0 +1,30 @@
+package com.am.innovations.validations;
+
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public interface Validators extends Predicate<Number> {
+
+	Predicate<Object> checkIfNotNull = input -> input != null;
+
+	BiPredicate<Number, Number> checkIfEqualsTo = (input, constraint) -> input.intValue() == constraint.intValue();
+	BiPredicate<Number, Number> checkIfGreaterThan = (input, constraint) -> input.intValue() > constraint.intValue();
+	BiPredicate<Number, Number> checkIfLessThan = (input, constraint) -> input.intValue() < constraint.intValue();
+
+	BiPredicate<Object[], Integer> checkIfNotNullAndSizeGreaterThan = (input, size) -> checkIfNotNull.test(input)
+			&& Validators.checkIfGreaterThan.test(input.length, size);
+
+	BiPredicate<Object[], Integer> checkIfSizeGreaterThan = (input, size) -> Validators.checkIfGreaterThan
+			.test(input.length, size);
+
+	interface Expressions {
+		/*
+		 * These are very simple expressions which should not reply on this
+		 * implementations. This is just for demo purpose.
+		 */
+		BiFunction<Integer, Integer, Integer> AVERAGE = (val1, val2) -> (val1 + val2) / LIMITS.TWO.getVal();
+		BiFunction<Integer, Integer, Integer> ADD = (val1, val2) -> (val1 + val2);
+		BiFunction<Integer, Integer, Integer> SUBSTRACT = (val1, val2) -> (val1 - val2);
+	}
+}
