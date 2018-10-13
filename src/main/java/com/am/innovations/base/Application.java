8377d25dd777cc8824698dd97679fd0d85f02be2
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-13 09:05:49 +0530

    Changes:1.) Disabled Test Cases Due to https://github.com/Akash-Mittal/commons/issues/1

--- src/main/java/com/am/innovations/base/Application.java
+++ src/main/java/com/am/innovations/base/Application.java
@@ -1,15 +1,15 @@
-package com.am.innovations.base;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@SpringBootApplication
-@EnableJpaAuditing
-public class Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-}
+// package com.am.innovations.base;
+//
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+// @SpringBootApplication
+// @EnableJpaAuditing
+// public class Application {
+//
+// public static void main(String[] args) {
+// SpringApplication.run(Application.class, args);
+// }
+//
+// }
--- src/main/java/com/am/innovations/base/UserRespository.java
+++ src/main/java/com/am/innovations/base/UserRespository.java
@@ -1,8 +1,7 @@
-package com.am.innovations.base;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRespository extends JpaRepository<User, Long> {
-}
+// package com.am.innovations.base;
+//
+// import org.springframework.data.jpa.repository.JpaRepository;
+//
+//// @Repository
+// public interface UserRespository extends JpaRepository<User, Long> {
+// }
