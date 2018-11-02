9b6e753a09c7d2d5a5156d86eedc513947470c14
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-11-02 16:19:04 +0530

    Changes;1.) Re-Factoring.

---
+++ src/main/java/com/am/innovations/enums/SYMBOLS.java
@@ -0,0 +1,24 @@
+package com.am.innovations.enums;
+
+public enum SYMBOLS {
+
+	UNDERSCORE("_", "_"), SPACE(" ", " "), COLON(":", ":"), SLASH("/", "/"), DOT(".", "\\."), BACKSLASH("\\", "\\"),
+	PIPE("|", "\\|");
+
+	String val;
+	String regexEqual;
+
+	private SYMBOLS(String val, String regexEqual) {
+		this.val = val;
+		this.regexEqual = regexEqual;
+	}
+
+	public String getVal() {
+		return val;
+	}
+
+	public String getRegexEqual() {
+		return regexEqual;
+	}
+
+}
