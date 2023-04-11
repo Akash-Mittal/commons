0855861e98d3e5fc091336bbb7757134674248af
Akash Mittal <mittal.akash.adam@gmail.com>
2023-04-11 23:54:21 +0200

    Changes: 1.) Updated SYMBOLS

--- src/main/java/com/am/innovations/enums/SYMBOLS.java
+++ src/main/java/com/am/innovations/enums/SYMBOLS.java
@@ -3,23 +3,24 @@ package com.am.innovations.enums;
 // All Symbols
 public enum SYMBOLS {
 
-	UNDERSCORE("_", "_"), SPACE(" ", " "), COLON(":", ":"), SLASH("/", "/"), DOT(".", "\\."), BACKSLASH("\\", "\\"),
-	PIPE("|", "\\|"), FORWARD_SLASH("/", "\\/"), QUESTION_MARK("?", "\\?"), SEMI_COLON(";", ";"), EQUALS("=", "=");
+    UNDERSCORE("_", "_"), SPACE(" ", " "), COLON(":", ":"), SLASH("/", "/"), DOT(".", "\\."), BACKSLASH("\\",
+            "\\"), PIPE("|", "\\|"), FORWARD_SLASH("/",
+                    "\\/"), QUESTION_MARK("?", "\\?"), SEMI_COLON(";", ";"), EQUALS("=", "="),HYPHEN("-", "-");
 
-	String val;
-	String regexEqual;
+    String val;
+    String regexEqual;
 
-	private SYMBOLS(String val, String regexEqual) {
-		this.val = val;
-		this.regexEqual = regexEqual;
-	}
+    private SYMBOLS(String val, String regexEqual) {
+        this.val = val;
+        this.regexEqual = regexEqual;
+    }
 
-	public String getVal() {
-		return val;
-	}
+    public String getVal() {
+        return val;
+    }
 
-	public String getRegexEqual() {
-		return regexEqual;
-	}
+    public String getRegexEqual() {
+        return regexEqual;
+    }
 
 }
